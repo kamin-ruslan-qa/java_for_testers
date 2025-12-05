de88b0a38451eb482b39c50189497cefd6b76834
ruslan222 <kaminruslan@yandex.ru>
2025-12-05 10:57:18 +0400

    выполнено изменение кода, по замечанием

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -9,37 +9,27 @@ public class Triangle {
         b = sideB;
         c = sideC;
     }
-
-
-    public static void main(String[] args) {
-        printperimetrTriangle(7, 7, 7);
-    }
-
-
-    //формула вычисления периметра P = a + b + c
-    private static void printperimetrTriangle(double a, double b, double c) {
-        System.out.println("Периметр треугольника: " + perimetrTriangle(a, b, c));
-
-        //Полупериметр рассчитывается как: p = (a + b + c) / 2
-        System.out.println("Полупериметр: " + halfPrtimetr(a, b, c));
-
-        // Площадь по формуле Герона
-        System.out.println("Площадь треугольника: " + areaTriangle(a, b, c));
-    }
-
-    public static double halfPrtimetr(double a, double b, double c) {
-        double p = (a + b + c) / 2.0;
-        return p;
-    }
-
-
-    public static double areaTriangle(double a, double b, double c) {
-        double p = halfPrtimetr(a, b, c);
-        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return area;
-    }
-    public static double perimetrTriangle(double a, double b, double c) {
+    public double getPerimeter() {
         return a + b + c;
     }
 
+    public double getHalfPerimeter() {
+        return getPerimeter() / 2.0;
+    }
+
+    public double getArea() {
+        double p = getHalfPerimeter();
+
+        // Формула Герона
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return area;
+    }
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(7, 7, 7);
+
+        System.out.println("Периметр треугольника: " + triangle.getPerimeter());
+        System.out.println("Полупериметр: " + triangle.getHalfPerimeter());
+        System.out.println("Площадь треугольника:: " + triangle.getArea());
+    }
 }
