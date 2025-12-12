3a1f0eb7cc4a30eea05f17d7c5b42890b9018c2c
4 <ruslan.kamin25@gmail.ru>
2025-12-12 17:42:40 +0400

    метод getClassNumber возвращает число больше 45.
    Тест 2 метод getClassNumber возвращает число больше 45.

--- sandbox/src/main/java/MainClass.java
+++ sandbox/src/main/java/MainClass.java
@@ -1,6 +1,13 @@
 public class MainClass {
+
     public static void main(String[] args) {
+
     }
+    private int class_number = 20;
+    public int getClassNumber() {
+        return class_number;
+    }
+
     public int getLocalNumber(){
         return 14;
     }
--- sandbox/src/test/java/MainClassTest.java
+++ sandbox/src/test/java/MainClassTest.java
@@ -11,7 +11,13 @@ public class MainClassTest {
             System.out.println("Тест пройден");
 
             Assert.assertTrue("Ответ должен содержать число 14", number == mainClass.getLocalNumber());
-
+        }
+        @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        int result = mainClass.getClassNumber();
+        Assert.assertTrue("Метод getClassNumber должен возвращать число больше 45, а вернул " + result,
+                result > 45);
 
         }
     }
